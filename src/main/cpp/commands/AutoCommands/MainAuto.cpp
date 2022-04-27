b57dbb5cd607705cc5f59d15168024e8804d5d73
ACERobotics <>
2022-04-26 17:46:37 -0700

    Working PID Turn

--- src/main/cpp/commands/AutoCommands/MainAuto.cpp
+++ src/main/cpp/commands/AutoCommands/MainAuto.cpp
@@ -2,5 +2,10 @@
 
 MainAuto::MainAuto(WestCoastDrive* p_Drive) : mp_Drive{p_Drive} {
     // NOT SAFE AT ALL
-    AddCommands(TurnToAnglePID{mp_Drive, 90_deg});
+    AddCommands(frc2::PrintCommand("Drive"), 
+    DriveStraight(mp_Drive, 12, 0.3), 
+    frc2::PrintCommand("Turn"), 
+    TurnToAnglePID{mp_Drive, 180_deg}, 
+    frc2::PrintCommand("Drive"), 
+    DriveStraight(mp_Drive, 12, 0.3));
 }
