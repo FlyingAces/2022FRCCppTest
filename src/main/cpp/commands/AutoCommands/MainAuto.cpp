7d5bc54a85ab714cb8a06d60b3bdcfb65c2ec703
ACERobotics <>
2022-04-26 17:59:08 -0700

    SAFE ISH

--- src/main/cpp/commands/AutoCommands/MainAuto.cpp
+++ src/main/cpp/commands/AutoCommands/MainAuto.cpp
@@ -1,7 +1,7 @@
 #include "commands/AutoCommands/MainAuto.h"
 
 MainAuto::MainAuto(WestCoastDrive* p_Drive) : mp_Drive{p_Drive} {
-    // NOT SAFE AT ALL
+    // SAFE ISH
     AddCommands(frc2::PrintCommand("Drive"), 
     DriveStraight(mp_Drive, 12, 0.3), 
     frc2::PrintCommand("Turn"), 
