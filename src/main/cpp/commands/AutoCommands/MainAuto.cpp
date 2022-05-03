251dc3d04c60b64a6d87ed6534abe2dad9fd8b79
ACERobotics <>
2022-05-03 12:20:11 -0700

    No more drive distance profiled

--- src/main/cpp/commands/AutoCommands/MainAuto.cpp
+++ src/main/cpp/commands/AutoCommands/MainAuto.cpp
@@ -3,9 +3,6 @@
 MainAuto::MainAuto(WestCoastDrive* p_Drive) : mp_Drive{p_Drive} {
     // SAFE ISH
     AddCommands(frc2::PrintCommand("Drive"), 
-    DriveStraight(mp_Drive, 12, 0.3), 
-    frc2::PrintCommand("Turn"), 
-    TurnToAnglePID{mp_Drive, 180_deg}, 
-    frc2::PrintCommand("Drive"), 
-    DriveStraight(mp_Drive, 12, 0.3));
+    DriveStraight(mp_Drive, 3, 0.35), 
+    frc2::PrintCommand("END"));
 }
